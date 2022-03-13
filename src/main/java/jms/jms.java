a82f2856d05c25c0777ac76162607a41d9c4c0d9
arturoBatistute <arturo.batistute@gmail.com>
2022-03-13 16:55:46 -0400

    Update the messaging consumer class.

--- src/main/java/jms/jms.java
+++ src/main/java/jms/jms.java
@@ -4,23 +4,49 @@ import java.util.Scanner;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class jms {
 
+	private static Session session;
+	private static Connection connection;
+	private static Destination destinationQueue;
+	private static InitialContext context;
+
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws NamingException, JMSException {
 
-		final InitialContext context = new InitialContext();
+		context = new InitialContext();
+
 		final ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-		final Connection connection = factory.createConnection();
+
+		connection = factory.createConnection();
+		destinationQueue = (Destination) context.lookup("test");
 
 		connection.start();
 
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		final MessageConsumer messageConsumer = session.createConsumer(destinationQueue);
+
+		final Message message = messageConsumer.receive();
+
+		System.out.println(message);
+
 		new Scanner(System.in).nextLine();
 
+		closeConnections();
+	}
+
+	private static void closeConnections() throws JMSException, NamingException {
+
+		session.close();
 		connection.close();
 		context.close();
 	}
